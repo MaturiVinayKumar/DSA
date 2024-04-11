8c6e794dbef9d1a528aba4fe42b1d6e9485d024b
MaturiVinayKumar <95517729+MaturiVinayKumar@users.noreply.github.com>
2024-04-11 13:14:19 +0530

    Create Reverse_an_array_using_in-built.py

---
+++ Recursion/Reverse_an_array_using_in-built.py
@@ -0,0 +1,9 @@
+import array as arr
+
+# Let's assume you have an array
+array = arr.array('i', [1, 2, 3, 4, 5])
+
+# To reverse the array in Python, you can use the reverse() function
+array.reverse()
+
+print(array)  # Output: array('i', [5, 4, 3, 2, 1])
