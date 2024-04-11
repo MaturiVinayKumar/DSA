645af9ef062051c53f4a25e88bbf460e99d7733a
MaturiVinayKumar <95517729+MaturiVinayKumar@users.noreply.github.com>
2024-04-11 13:16:49 +0530

    Create fibinocci.py

---
+++ Recursion/fibinocci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+# Test the function
+print(fibonacci(10))
